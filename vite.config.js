3d4aa3d068227b8d550714cff003969831591d0d
qisili-66 <2216423932@qq.com>
2026-04-16 20:52:28 +0800

    elementUIPlus按需配置

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+//element-plus按需引入
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    }),
     vueDevTools(),
   ],
   resolve: {
