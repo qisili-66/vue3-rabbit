53cc43957ba24d576955085abfa1f9e28c7360f4
qisili-66 <2216423932@qq.com>
2026-05-06 22:40:30 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue';
 import HomeNew from './components/HomeNew.vue';
 import HomeHot from './components/HomeHot.vue';
 import HomeProduct from './components/HomeProduct.vue';
+import HomePanel from './components/HomePanel.vue';
 
 </script>
 
@@ -16,5 +17,12 @@ import HomeProduct from './components/HomeProduct.vue';
         <HomeNew />
         <HomeHot />
         <HomeProduct />
-
+        <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱" />
+         <div>
+            我是新鲜好物的插槽
+         </div>
+        <HomePanel title="人气推荐" subTitle="热门推荐 不容错过" />
+            <div>
+                我是人气推荐的插槽
+            </div>
 </template>
