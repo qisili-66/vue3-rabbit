cfc366f4ae6f910336fb6b8626d5d6970c45eda1
qisili-66 <2216423932@qq.com>
2026-05-29 09:57:15 +0800

    自定义校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -6,6 +6,7 @@ import { ref } from 'vue';
 const form = ref({
   account: '',
   password: '',
+  agree: true
 })
 
 //2.准备规则对象
@@ -16,8 +17,22 @@ const rules={
   password:[
     {required:true,message:'密码不能为空',trigger:'blur'},
      {min:6,max:14,message:'密码必须在6-14个字符之间',trigger:'blur'}
-  ]
-}
+  ],
+  agree:[
+    { validator: (rule, value, callback) => {
+      //自定义校验逻辑
+      //勾选就通过，不勾选就不通过
+      if(value){
+        callback()
+      }else{
+        callback(new Error('请勾选协议'))
+      }
+    }
+      }
+    ]
+    }
+    
+
 </script>
 
 <template>
@@ -48,8 +63,9 @@ const rules={
                         <el-form-item prop="password" label="密码">
                             <el-input v-model="form.password" type="password" />
                         </el-form-item>
-                        <el-form-item label-width="22px">
-                            <el-checkbox size="large">我已同意隐私条款和服务条款</el-checkbox>
+                        <el-form-item prop="agree" label-width="22px">
+                            <el-checkbox size="large" v-model="form.agree">我已同意隐私条款和服务条款
+                            </el-checkbox>
                         </el-form-item>
                         <el-button size="large" class="subBtn">登录</el-button>
                     </el-form>
@@ -57,7 +73,6 @@ const rules={
             </div>
         </div>
     </section>
-    
     <footer class="login-footer">
         <div class="container">
             <p>
