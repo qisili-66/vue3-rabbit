c987e2db1d5815b936db1ceb8414d97d762a13d7
qisili-66 <2216423932@qq.com>
2026-06-01 21:20:12 +0800

    登录-使用token适配多模块显示

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,4 +1,14 @@
 <script setup>
+import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+
+const router = useRouter()
+const userStore = useUserStore()
+
+const confirm = () => {
+  // 退出登录逻辑
+  router.push('/login')
+}
 </script>
 
 <template>
@@ -6,12 +16,8 @@
     <div class="container">
       <ul>
         <!--多模版渲染区分登录状态和非登录状态-->
-        <template v-if="false">
-          <li>
-            <a href="javascript:;">
-              <i class="iconfont icon-user"></i>
-            </a>
-          </li>
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消" @confirm="confirm">
               <template #reference>
@@ -22,9 +28,10 @@
           <li><RouterLink to="/member/order">我的订单</RouterLink></li>
           <li><RouterLink to="/member/user">会员中心</RouterLink></li>
         </template>
+        <!--非登录状态-->
         <template v-else>
           <li>
-            <a href="javascript:;" @click="$router.push('/login')">请先登录</a>
+            <a href="javascript:;" @click="router.push('/login')">请先登录</a>
           </li>
           <li><a href="javascript:;">帮助中心</a></li>
           <li><a href="javascript:;">关于我们</a></li>
@@ -33,18 +40,6 @@
     </div>
   </nav>
 </template>
-
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const confirm = () => {
-  // 退出登录逻辑
-  // 例如清除token，重定向到登录页
-  router.push('/login')
-}
-</script>
 
 <style scoped lang="scss">
 .app-topnav {
