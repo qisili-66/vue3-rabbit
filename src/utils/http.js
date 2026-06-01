d962c09f915c5cbf90e3803d1838d7d58f91a3f0
qisili-66 <2216423932@qq.com>
2026-06-01 21:28:31 +0800

    登录-token拦截器的携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
+import{ useUserStore } from '../stores/user'
 
 const httpInstance = axios.create({
   baseURL: '/api',
@@ -11,6 +12,13 @@ const httpInstance = axios.create({
 //拦截器
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  //1、从pinia中获取token数据
+const userStore = useUserStore()
+  //2、将token添加到请求头中
+  const token= userStore.userInfo.token
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e=> Promise.reject(e))
 
